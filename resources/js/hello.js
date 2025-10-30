9ed8866cd540442a5d99ec88c9895a61e77789db
Andrew Sykes <andrewsykes@btinternet.com>
2025-10-30 11:06:25 +0000

    js

--- resources/js/hello.js
+++ resources/js/hello.js
@@ -1,38 +1,60 @@
 let service = document.querySelector(".services"),
   log = console.log,
   articles = service.getElementsByTagName("article"),
-  f = (node) => node.parentNode.appendChild(node),
-  delay = (node) => () => node.parentNode.appendChild(node),
-  func = (f, t) => setTimeout(f, t),
-  defer = (f, t) => () => f(t);
+  i = articles.length,
+  alpha = ["a", "b", "c", "d", "e", "f"];
+
+while (i--) {
+  articles[i].className = alpha[i];
+}
 
 let element = articles[0],
   next = element,
   start,
-  pc = 343,
+  pc = 34333,
   inc = 0,
-  t = 200;
+  int = 0.1,
+  t = 1000;
+
+function flip(element) {
+  element.parentNode.appendChild(element);
+  element.style.marginLeft = 0;
+  return element.parentNode.firstElementChild;
+}
 
 function step(timestamp) {
+  if (!start && inc) {
+    t = inc / 100;
+    int = 0.5;
+  }
   if (start === undefined) {
     start = timestamp;
   }
   const elapsed = timestamp - start,
-    shift = Math.min(0.1 * elapsed, t);
-  //log(timestamp);
-  if (shift < t) {
-    requestAnimationFrame(step);
+    shift = Math.min(int * elapsed, t);
+
+  if (inc) {
+    if (shift < inc / 100) {
+      let px = `-${shift / 10}%`;
+      element.style.marginLeft = px;
+      requestAnimationFrame(step);
+    } else {
+      inc = 0;
+      t = 1000;
+      start = undefined;
+      element = flip(element);
+      if (element !== next) {
+        requestAnimationFrame(step);
+      }
+    }
   } else {
-    //inc = pc;
-    start = undefined;
-    element.style.marginLeft = `-34.33%`;
-    element.parentNode.appendChild(element);
-    element.style.marginLeft = 0;
-    element = element.parentNode.firstElementChild;
-    if (element !== next) {
+    if (shift < t) {
+      requestAnimationFrame(step);
+    } else {
+      inc = pc;
+      start = undefined;
       requestAnimationFrame(step);
     }
   }
 }
-
 requestAnimationFrame(step);
