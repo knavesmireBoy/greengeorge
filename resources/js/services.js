b73d4fa17ddea4c1e0de97816eaea45add192538
Andrew Sykes <andrewsykes@btinternet.com>
2025-11-12 16:41:47 +0000

    map

--- resources/js/services.js
+++ resources/js/services.js
@@ -21,13 +21,15 @@ function setServicesBgImage(nodes, klasses) {
   }
 }
 
-function myslider(hook, gang, i, delay) {
+function myslider(hook, gang, i, delay, duration = 500) {
   const f = curry2(utils.getComputedStyle)("background-image"),
     urls = gang.map(f),
     exec = curry22(preMove)("add"),
     undo = curry2(preMove)("remove"),
     n = urls.length,
-    j = i;
+    j = i,
+    sixty = 'center 60%';
+    pos = {8: 'center 70%', 6: sixty, 14: sixty, 15: 'center 65%', 16: sixty, 12: 'center 55%', 10: 'center 40%'};
 
   let k = 0,
     t = 1;
@@ -42,6 +44,13 @@ function myslider(hook, gang, i, delay) {
   }
 
   return function play(e) {
+
+    /*
+    starter = starter === undefined ? timestamp : starter;
+    const elapsed = timestamp - start,
+      shift = Math.min(0.1 * elapsed, duration);
+      */
+
     if (e) {
       if (e.target.nodeName !== "FIGURE") {
         return;
@@ -51,20 +60,22 @@ function myslider(hook, gang, i, delay) {
         clearTimeout(t);
         t = 0;
         activate(hook);
-        //undo(hook);
         return;
       } else {
         deactivate(hook);
-        //exec(hook)
         t = -1;
       }
     }
     let cb = () => {
-        hook.insertBefore(hook.lastElementChild, hook.firstElementChild);
-        undo(hook);
-        i = i % n ? i : 0;
-        hook.firstElementChild.style.backgroundImage = urls[i++];
-        if(t) play();
+      hook.insertBefore(hook.lastElementChild, hook.firstElementChild);
+      undo(hook);
+      i = i % n ? i : 0;
+      if(pos[i]){
+        hook.firstElementChild.style.backgroundPosition = pos[i];
+      }
+      hook.firstElementChild.style.backgroundImage = urls[i++];
+      
+      if (t) play();
     };
     if (t) {
       setTimeout(exec(hook));
@@ -91,7 +102,9 @@ let inc = 0,
   t = 500,
   margins = [100, 52, 34.333],
   request,
-  start;
+  requester,
+  start,
+  starter;
 
 const meta = greenGeorge.meta,
   utils = greenGeorge.utils,
@@ -216,9 +229,8 @@ const meta = greenGeorge.meta,
       }
     };
   },
-  slideshow = myslider(aside, meta.toArray(figures), 2, 4000);
-
-step = stepper(articles[0], margins, validator(next), cb);
+  slideshow = myslider(aside, meta.toArray(figures), 2, 4000),
+  step = stepper(articles[0], margins, validator(next), cb);
 
 function controller(e) {
   let a = Array.prototype.slice.call(this.childNodes),
