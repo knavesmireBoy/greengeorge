6b90a9bce9adcb43802225fea2872d469efd1c81
Andrew Sykes <andrewsykes@btinternet.com>
2025-11-28 15:54:31 +0000

    change services2 to services

--- resources/js/services.js
+++ resources/js/services.js
@@ -3,184 +3,219 @@
 /* eslint-disable no-param-reassign */
 /*global greenGeorge: false */
 if (!window.greenGeorge) {
-    window.greenGeorge = {};
-  }
+  window.greenGeorge = {};
+}
 
-  function paint(node, val) {
-    node.style.backgroundColor = val;
-  }
-  
-  
-  function loop(element) {
-    element.parentNode.appendChild(element);
-    element.style.marginLeft = 0;
-    return element.parentNode.firstElementChild;
-  }
-  
-  function setServicesBgImage(nodes, klasses) {
-    let i = nodes.length;
-    if (i === klasses.length) {
-      while (i--) {
-        nodes[i].classList.add(klasses[i]);
-      }
-    }
-  }
+let inc = 0,
+  t = 500,
+  margins = [100, 52, 34.333],
+  request,
+  requester,
+  start,
+  starter;
 
-  function paint(node, val) {
-    node.style.backgroundColor = val;
-  }
-  
-  let inc = 0,
-    t = 500,
-    margins = [100, 52, 34.333],
-    request,
-    requester,
-    start,
-    starter;
-  
-  const meta = greenGeorge.meta,
-    utils = greenGeorge.utils,
-    getProp = (o, p) => o[p],
-    comp = meta.compose,
-    sub = (o, p, m, v) => o[p][m](v),
-    prevoke = (p, v) => (o, m) => sub(o, p, m, v),
-    curry2 = meta.curryRight(2),
-    curry22 = meta.curryRight(2, true),
-    getTarget = curry2(getProp)("currentTarget"),
-    preActive = prevoke("classList", "pause"),
-    preMove = prevoke("classList", "mv"),
-    activate = curry2(preActive)("add"),
-    deactivate = curry2(preActive)("remove"),
-    doActive = comp(activate, getTarget),
-    undoActive = comp(deactivate, getTarget),
-    doAlt = meta.doAlternate(),
-    finder = (nodes) => (node) => {
-      let i = 0,
-        l = nodes.length;
-      while (i < l) {
-        if (nodes[i] === node) {
-          break;
-        }
-        i++;
-      }
-      return i;
-    },
-    spotify = (nodes, values, cb) => (j) => {
-      let i = nodes.length,
-        [dflt, current] = values;
-      while (i--) {
-        if (i === j) {
-          cb(nodes[i], current);
-        } else {
-          cb(nodes[i], dflt);
-        }
-      }
-    },
-    resize = (margins, factor = 2.5) => {
-      let width = window.innerWidth > 0 ? window.innerWidth : screen.width,
-        int = 0;
-      margins = margins.map((n) => n * factor);
-  
-      if (width > 768) {
-        int = 1;
-      }
-      if (width > 1024) {
-        int = 2;
-      }
-      return [margins[int], factor];
-    },
-    service = document.querySelector(".services"),
-    control = document.getElementById("control"),
-    liveArticles = (service && service.getElementsByTagName("article")) || [],
-    articles = (service && service.querySelectorAll("article")) || [],
-    i = articles.length,
-    el = articles[0],
-    next = el,
-    validate = () => true,
-    validator = (a) => (b) => a !== b,
-    move = (node, val) => (node.style.marginLeft = val),
-    cycle = spotify(
-      control.getElementsByTagName("span"),
-      ["rgba(255,255,255, .2)", "white"],
-      paint
+var elapsed;
+//note meta etc.. avoid binding clashes from previous script
+const meta = greenGeorge.meta,
+  utils = greenGeorge.utils,
+  log = console.log,
+  ptL = meta.doPartial(),
+  defer = meta.doPartial(true),
+  compose = meta.compose,
+  compduo = (f1, f2) => compose(f2, f1),
+  composer = (f1, f2) => compose(f2, f1)(),
+  getprop = (o, p) => o[p],
+  pass = (f) => (arg) => {
+    f(arg);
+    return arg;
+  },
+  invk = (o, m, v) => o[m](v),
+  prevoke = (m) => (o, v) => o[m](v),
+  invok = (o, m, k, v) => o[m](k, v),
+  prepSubMethod = (p, v) => (o, m) => o[p][m](...v),
+  cu2 = meta.curryRight(2),
+  hifactory = prepSubMethod("classList", ["hi"]),
+  transformfactory = prepSubMethod("classList", ["transform", "transit"]),
+  transformRevfactory = prepSubMethod("classList", ["transform"]),
+  transitfactory = prepSubMethod("classList", ["transit"]),
+  highlighter = {
+    exec: cu2(hifactory)("add"),
+    undo: cu2(hifactory)("remove"),
+  },
+  transformer = {
+    exec: cu2(transformfactory)("add"),
+    undo: cu2(transformfactory)("remove"),
+  },
+  transformerRev = {
+    exec: compose(
+      cu2(transformRevfactory)("add"),
+      pass(cu2(transitfactory)("remove"))
     ),
-    cb = comp(cycle, finder(articles)),
-    stepper = (
-      element,
-      data,
-      validator,
-      callback,
-      dur = 500,
-      inc = 0,
-      start = undefined
-    ) => {
-      let [travel, factor] = resize(data, 1),
-        duration;
-  
-      return (timestamp) => {
-        if (!start && inc) {
-          duration = travel;
-        } else if (!inc) {
-          duration = dur;
-        }
-        start = start === undefined ? timestamp : start;
-  
-        const elapsed = timestamp - start,
-          shift = Math.min(0.1 * elapsed, duration);
-  
-        if (inc) {
-          if (shift < inc) {
-            move(element, `-${shift / factor}%`);
-            request = requestAnimationFrame(step);
-          } else {
-            inc = 0;
-            start = undefined;
-            element = loop(element);
-            callback(element);
-            if (validator(element)) {
-              request = requestAnimationFrame(step);
-            } else {
-              cancelAnimationFrame(request);
-            }
-          }
-        } else {
-          if (shift < duration) {
-            request = requestAnimationFrame(step);
-          } else {
-            inc = travel;
-            start = undefined;
-            request = requestAnimationFrame(step);
-          }
-        }
-      };
-    },
-    step = stepper(articles[0], margins, validator(next), cb);
-  
-  function controller(e) {
-    let a = meta.toArray(this.childNodes).filter( n => n.nodeName === 'SPAN'),
-      live = meta.toArray(liveArticles).filter( n => n.nodeName === 'ARTICLE'),
-      parent = liveArticles[0].parentNode,
-      f = finder(a),
-      i = f(e.target),
-      j = 0,
-      article;
-  
-    if (e.target !== this) {
-      cycle(i);
-      cancelAnimationFrame(request);
-      article = articles[i];
-      j = live.indexOf(article);
-      i = 0;
-      while (i < j) {
-        parent.appendChild(live[i]);
-        i++;
+    undo: compose(
+      cu2(transitfactory)("add"),
+      pass(cu2(transformRevfactory)("remove"))
+    ),
+  },
+  transit = {
+    exec: cu2(transitfactory)("add"),
+    undo: cu2(transitfactory)("remove"),
+  },
+  animed = (t, flag = false) => {
+    if (flag) {
+      if (t < 7) {
+        return 0;
       }
+      if (t >= 7 && t < 14) {
+        return 1;
+      }
+      if (t >= 14 && t < 21) {
+        return 2;
+      }
+      return 3;
+    } else {
+      if (t < 7) {
+        return 3;
+      }
+      if (t >= 7 && t < 14) {
+        return 2;
+      }
+      if (t >= 14 && t < 21) {
+        return 1;
+      }
+      return 0;
     }
-  }
-  if (el) {
-    request = requestAnimationFrame(step);
-    control.addEventListener("click", controller);
-    cb(el);
-    setServicesBgImage(liveArticles, ["a", "b", "c", "d", "e", "f"]);
-  }
-  
+  },
+  service = document.querySelector(".services"),
+  control = document.getElementById("control"),
+  livespans = control.getElementsByTagName("span"),
+  section = meta.$Q(".services"),
+  container = meta.byTagScope(section)("div"),
+  appender = ptL(invk, container, "appendChild"),
+  inserter = ptL(invok, container, "insertBefore"),
+  inserterControl = ptL(invok, control, "insertBefore"),
+  getRefNode = ptL(
+    composer,
+    defer(getprop, container, "firstChild"),
+    defer(utils.getNextElement)
+  ),
+  getLastNode = defer(
+    composer,
+    defer(getprop, container, "lastChild"),
+    defer(utils.getPrevElement)
+  ),
+  insertB4 = compose(
+    ptL(composer, getRefNode),
+    cu2(composer)(inserter),
+    getLastNode
+  ),
+  getControlRefNode = ptL(
+    composer,
+    defer(getprop, control, "firstChild"),
+    defer(utils.getNextElement)
+  ),
+  getControlLastNode = defer(
+    composer,
+    defer(getprop, control, "lastChild"),
+    defer(utils.getPrevElement)
+  ),
+  spotshifterbak = compose(
+    ptL(composer, getControlRefNode),
+    cu2(composer)(inserterControl),
+    getControlLastNode
+  ),
+  appendTo = defer(composer, getRefNode, appender),
+  spotshifter = compose(
+    ptL(invk, control, "appendChild"),
+    defer(utils.getNextElement, control.firstChild)
+  ),
+  domino = (n, fns) => (x = 0) => {
+    let i = n,
+    fn = fns[x];
+    while (n--) {
+      fn();
+    }
+    n = i;
+  };
+
+function testi() {
+  elapsed = Date.now();
+}
+
+function play(callback) {
+  var contains = cu2(hifactory)("contains"),
+    serv = meta.$Q(".services"),
+    arts = serv.getElementsByTagName("article");
+
+  return function (e) {
+    function tick(action, state, f = () => true) {
+      return function (t, r, i, k) {
+        let j = 0;
+        f();//this would be insertB4 if going back, needs to run BEFORE, transform/transit classes are applied
+        while (arts[j]) {
+          state.exec(arts[j]);
+          j++;
+        }
+        setTimeout(function () {
+          let j = 0;
+          while (arts[j]) {
+            state.undo(arts[j]);
+            j++;
+          }
+        }, t);
+
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(action(r, i, k));
+          }, t);
+        });
+      };
+    }
+
+    const tgt = e.target,
+      mover = (i, rev) => {
+        appendTo();//runs AFTER transform
+        i--;
+        return [i, rev];
+      },
+      mova = (i, rev) => {
+        i--;
+        return [i, rev];
+      };
+
+    let reqst = 0,
+      offset = 0,
+      dur = 600,
+      rev = 0,
+      mytimer = tick(mover, transformer);
+
+    if (this.nodeType === 1 && tgt.nodeName === "SPAN") {
+      while (livespans[reqst] !== tgt) {
+        reqst++;
+      }
+
+      while (!contains(livespans[offset])) {
+        offset++;
+      }
+
+      if (reqst < offset) {
+        mytimer = tick(mova, transformerRev, insertB4);
+        rev = 1;
+      }
+      reqst -= offset;
+      reqst = Math.abs(reqst);
+      async function func(f, t, ...args) {
+        const result = await f(t, ...args),
+          [i, rev] = result,
+          next = meta.pApply(func, f, t, i, rev);
+        callback(rev);
+        if (i > 0) {
+          setTimeout(next, t);
+        }
+      }
+      func(mytimer, dur, reqst, rev);
+    }
+  };
+}
+
+highlighter.exec(livespans[0]);
+meta.$("control").addEventListener("click", play(domino(5, [spotshifter, spotshifterbak])));
