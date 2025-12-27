b80cfa1bbfc43535d0e9239bb1841690ecb8e825
Andrew Sykes <andrewsykes@btinternet.com>
2025-12-27 14:04:34 +0000

    refactor

--- resources/js/map2.js
+++ resources/js/map2.js
@@ -9,7 +9,6 @@ const tagTester = (name) => {
   log = pass(console.log),
   invoke = f => f(),
   invokeV = (o, m, v) => o[m](v),
-  invokeKV = (o, m, k, v) => o[m](k, v),
   prevoker = (m, k) => (o, v) => o[m](k, v),
   prevoke = (m) => (o, v) => o[m](v),
   getta = (o, p) => o[p],
@@ -44,20 +43,20 @@ const tagTester = (name) => {
   query = doDoc("querySelector"),
   create = doDoc("createElement"),
   doText = doDocDefer("createTextNode"),
+  setAttrs = ptL(prevoker, 'setAttribute'),
+  mapsrc = "https://www.openstreetmap.org/export/embed.html?bbox=-0.08222579956054689%2C51.540383176643516%2C-0.046992301940917976%2C51.55831649890036&amp;layer=mapnik",
   hook = query(".map"),
   para = query("#map"),
   link = create("a"),
-  setAttrs = ptL(prevoker, 'setAttribute'),
-  setHref = prevoker('setAttribute', 'href'),
-  setWidth = curry2(prevoker('setAttribute', 'width'))(425),
-  setHeight = curry2(prevoker('setAttribute', 'height'))(350),
-  setTarget = curry2(prevoker('setAttribute', 'target'))('_blank'),
-  setSource = curry2(prevoker('setAttribute', 'src'))("https://www.openstreetmap.org/export/embed.html?bbox=-0.08222579956054689%2C51.540383176643516%2C-0.046992301940917976%2C51.55831649890036&amp;layer=mapnik"),
+  setWidth = curry2(setAttrs('width'))(425),
+  setHeight = curry2(setAttrs('height'))(350),
+  setTarget = curry2(setAttrs('target'))('_blank'),
+  setSource = curry2(setAttrs('src'))(mapsrc),
   prependLink = curry3(invokeV)(link)('appendChild'),
-  setLinkHref = curry2(setHref)("https://www.openstreetmap.org/?#map=15/51.54935/-0.06461"),
+  setLinkHref = curry2(setAttrs('href'))("https://www.openstreetmap.org/?#map=15/51.54935/-0.06461"),
   appendToSection = ptL(invokeV, hook, 'appendChild'),
-  prepend = prevoke('appendChild'), 
-  makeIframe = compose(setTarget, curry2(getta)('parentNode'), invoke, ptL(composer, doText("View Larger Map")), ptL(prepend), pass(setLinkHref), prependLink, curry2(getta)('parentNode'), appendToSection, pass(setSource), pass(setWidth), pass(setHeight), create);
+  awaitTextNode = prevoke('appendChild'), 
+  makeIframe = compose(invoke, ptL(composer, doText("View Larger Map")), ptL(awaitTextNode), pass(setTarget), pass(setLinkHref), prependLink, curry2(getta)('parentNode'), appendToSection, pass(setSource), pass(setWidth), pass(setHeight), create);
 
 
   makeIframe('iframe');
